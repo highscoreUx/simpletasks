a173a62dc4f8d50b47a41fd1f3783dab001477f3
highscoreux <boyesiji@gmail.com>
2024-09-05 16:51:14 +0100

    feat: add user and task models

--- server/src/models/Task.ts
+++ server/src/models/Task.ts
@@ -1,8 +1,10 @@
-interface Itask {
+import mongoose, { Document } from "mongoose";
+
+interface Itask extends Document {
 	title: string;
 	createdBy: string;
 	assignedTo?: string[];
-	taskStatus?:
+	taskStatus:
 		| "Not Started"
 		| "in Progress"
 		| "Submitted"
@@ -16,3 +18,47 @@ interface Itask {
 	completedOn?: string;
 	comments?: string[];
 }
+
+const taskSchema = new mongoose.Schema<Itask>(
+	{
+		title: { type: String, required: [true, "Title cannot be empty"] },
+		createdBy: { type: String, required: [true, "Creator cannot be empty"] },
+		assignedTo: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		taskStatus: {
+			type: String,
+			enum: ["Not Started", "in Progress", "Submitted", "Cancelled", "Expired"],
+			default: "Not Started",
+		},
+		assignedOn: {
+			type: String,
+			required: [true, "Date Assigned cannot be empty"],
+		},
+		dueDate: { type: String },
+		description: { type: String },
+		priority: {
+			type: String,
+			enum: ["Low", "Medium", "High", "None"],
+			default: "None",
+		},
+		tags: [
+			{
+				type: String,
+			},
+		],
+		completedOn: String,
+		comments: [
+			{
+				type: String,
+			},
+		],
+	},
+	{ timestamps: true },
+);
+const Task = mongoose.model<Itask>("Task", taskSchema);
+
+export default Task;
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document } from "mongoose";
+import bcrypt from "bcryptjs";
 
 interface Iuser extends Document {
 	email: string;
@@ -8,12 +9,46 @@ interface Iuser extends Document {
 	isActive: boolean;
 }
 
-const userSchema = new mongoose.Schema<Iuser>({
-	email: {
-		type: String,
-		required: [true, "Please provide email address"],
+const userSchema = new mongoose.Schema<Iuser>(
+	{
+		email: {
+			type: String,
+			required: [true, "Please provide email address"],
+			trim: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Please provide a valid password"],
+		},
+		role: {
+			type: String,
+			enum: ["user", "admin"],
+			default: "user",
+		},
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+		isActive: {
+			type: Boolean,
+			default: true,
+		},
 	},
+	{ timestamps: true },
+);
+
+userSchema.pre<Iuser>("save", async function () {
+	if (this.isModified("password") || this.isNew) {
+		try {
+			const salt = await bcrypt.genSalt(10);
+			this.password = await bcrypt.hash(this.password, salt);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 });
+
 const User = mongoose.model<Iuser>("User", userSchema);
 
 export default User;
